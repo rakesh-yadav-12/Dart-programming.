12be20181ad6d79bbde6fd5eba3f9583507570a6
Rakesh Yadav <ry7955724@gmail.com>
2024-12-18 13:43:39 +0530

    Add files via upload

---
+++ 2.simple_arthematic_opeeration.dart
@@ -0,0 +1,12 @@
+void main() {
+  int a = 10;
+  int b = 5;
+//sum operation
+  print(a + b);
+//difference operation
+  print(a - b);
+  //product operation
+  print(a*b);
+  //Quotient operation
+  print(a/b);
+}
